c7f59792e968bb669c05b336d52cb2c5016599da
Rentib <sbitner420@tutanota.com>
2022-05-05 15:19:50 +0200

    pure alpha beta negamax

--- src/search.c
+++ src/search.c
@@ -1,18 +1,83 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "chesslib.h"
+#include "evaluate.h"
 #include "misc.h"
 #include "movegen.h"
+#include "moveorder.h"
 #include "position.h"
+#include "search.h"
 
+static int negamax(Position *pos, int alpha, int beta, int depth);
 static uint64_t perft_help(Position *pos, int depth);
 static inline void print_move(Move m);
+
+SearchData data;
+
+static int
+negamax(Position *pos, int alpha, int beta, int depth)
+{
+  int value = -INFINITY;
+  int ksq = pos->ksq[pos->turn];
+  U64 checkers = attackers_to(pos, ksq, ~pos->empty) & pos->color[!pos->turn];
+  Move *m, *last, move_list[256];
+  Position tmp; /* used instead of undo move */
+  if (data.ply >= MAX_PLY) return evaluate(pos);
+
+  /* dont end search on check */
+  if (depth <= 0) {
+    if (!checkers)
+      return evaluate(pos);
+    depth = 1;
+  }
+
+  data.nodes++;
+
+  last = generate_moves(ALL, move_list, pos);
+  last = process_moves(pos, move_list, last);
+
+  /* checkmate or stalemate */
+  if (move_list == last)
+    return checkers ? data.ply - MATE_VALUE : 0;
+
+  for (m = move_list; m != last; m++) {
+    tmp = *pos;
+    do_move(&tmp, *m);
+    data.ply++;
+
+    value = -negamax(&tmp, -beta, -alpha, depth - 1);
+
+    data.ply--;
+    if (value >= beta)
+      return beta;
+    if (value > alpha) {
+      if (data.ply == 0) data.best_move = *m;
+      alpha = value;
+    }
+  }
+
+  return alpha;
+}
 
 void
 search(Position *pos, int depth)
 {
+  int alpha = -INFINITY, beta = INFINITY;
+
+  data.nodes = 0ULL;
+  data.ply = 0;
+  data.best_move = MOVE_NONE;
+  data.beg_time = get_time();
+  data.score = -negamax(pos, alpha, beta, depth);
+  data.end_time = get_time();
+
+  printf("info depth %d score %d nodes %lu time %d pv ...",
+         depth, data.score, data.nodes, data.end_time - data.beg_time);
+  printf("\n");
+
   printf("bestmove ");
-  print_move(make_move(SQ_E2, SQ_E4));
+  print_move(data.best_move);
   printf("\n");
 }
 
@@ -56,23 +121,24 @@ perft(Position *pos, int depth)
   printf("\nNodes searched: %lu (%dms)\n\n", nodes_searched, get_time() - t);
 }
 
-static const char *sq_to_str[] = {
-  "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
-  "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-  "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
-  "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
-  "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
-  "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
-  "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-  "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
-  "-",
-};
-
-static const char *pc_to_str[] = { "p", "n", "b", "r", "q", "k" };
-
-static inline void print_move(Move m)
+static inline void
+print_move(Move m)
 {
-  printf("%s%s%s", sq_to_str[from_sq(m)], sq_to_str[to_sq(m)],
+  static const char *pc_to_str[] = { "p", "n", "b", "r", "q", "k" };
+
+  const char *t[] = {
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+    "--",
+  };
+
+  printf("%s%s%s", t[from_sq(m)],t[to_sq(m)],
                    type_of(m) == PROMOTION ?
                    pc_to_str[KNIGHT + promotion_type(m)] : "");
 }
--- src/search.h
+++ src/search.h
@@ -2,7 +2,23 @@
 #ifndef __SEARCH_H__
 #define __SEARCH_H__
 
+#include "chesslib.h"
 #include "position.h"
+
+#define MAX_PLY    64
+#define INFINITY   50000
+#define MATE_VALUE 49000
+
+typedef struct {
+  uint64_t nodes;
+  int      ply;
+  int      beg_time;
+  int      end_time;
+  int      score;
+  Move     best_move;
+} SearchData;
+
+extern SearchData data;
 
 void perft(Position *pos, int depth);
 void search(Position *pos, int depth);
